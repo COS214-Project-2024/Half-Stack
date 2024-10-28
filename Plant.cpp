0d86e3c5a56ad454fb3ae7f0c049dbda95ccbd4f
DevanDewet <161315233+DevanDewet@users.noreply.github.com>
2024-10-28 19:36:38 +0200

    Skeleton code for Plant.cpp

---
+++ Plant.cpp
@@ -0,0 +1,41 @@
+#include "Plant.h"
+
+Plant::Plant(int num, std::string l)
+{
+	
+}
+
+void Plant::Build()
+{
+
+}
+
+PowerPlant::PowerPlant(int num, std::string l) : Plant(num, l)
+{
+	
+}
+
+void PowerPlant::generate()
+{
+	
+}
+
+WaterPlant::WaterPlant(int num, std::string l) : Plant(num, l)
+{
+	
+}
+
+void WaterPlant::generate()
+{
+	
+}
+
+MaterialsPlant::MaterialsPlant(int num, std::string l) : Plant(num, l)
+{
+	
+}
+
+void MaterialsPlant::generate()
+{
+	
+}
