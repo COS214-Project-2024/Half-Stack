5a53db381ec36a4cc64f0329df43f5f75eaf99bf
Au5tin-R <srhung1231@gmail.com>
2024-10-28 18:57:58 +0200

    Update Employment.cpp
    Added the functions to toggle the employment status changes

--- Employment.cpp
+++ Employment.cpp
@@ -5,7 +5,17 @@ std::string Employed::getStatus()
     return "";
 }
 
+Employment *Employed::toggleStatus()
+{
+    return new Unemployed();
+}
+
 std::string Unemployed::getStatus()
 {
     return "";
+}
+
+Employment *Unemployed::toggleStatus()
+{
+    return new Employed();
 }
