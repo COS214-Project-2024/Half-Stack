984eab1abc2ade5135eb8a22e70ba1ca3bed0188
Au5tin-R <srhung1231@gmail.com>
2024-10-28 19:07:49 +0200

    Update Citizen.cpp
    Fixed the Constructor.
    Finished the startwork() and getjob() functions.

--- Citizen.cpp
+++ Citizen.cpp
@@ -4,6 +4,8 @@ Citizen::Citizen(std::string name, int age)
 {
     this->name = name;
     this->age = age;
+    this->employment = new Unemployed();
+    this->satisfaction = new Neutral();
 }
 
 void Citizen::payTax(std::string tax)
@@ -13,8 +15,18 @@ void Citizen::payTax(std::string tax)
 
 void Citizen::startWork()
 {
-
+    if (this->employment->getStatus() == "Employed")
+    {
+        std::cout << this->name << " is starting work." << std::endl;   //Start job if has job
+    }
+    else
+    {
+        this->employment = this->employment->toggleStatus();            //Get the job to start work.
+        std::cout << this->name << " now has the job and will start work." << std::endl;
+    }
+    
 }
+    
 
 void Citizen::goHome()
 {
@@ -43,5 +55,13 @@ void Citizen::setSatisfaction(Satisfaction* s)
 
 void Citizen::getJob()
 {
-    
+    if (this->employment->getStatus() == "Unemployed")
+    {
+        std::cout << this->name << " obtaind a job." << std::endl;
+        this->employment = employment->toggleStatus();
+    }
+    else
+    {
+        std::cout << this->name << " already has a job. " << this->name << "cannot obtain a job if he has one already." << std::endl;
+    }
 }
