4351d982d2446a345a871202027e97301b908b22
Au5tin-R <srhung1231@gmail.com>
2024-10-28 18:38:18 +0200

    All Citizen Functions for the class (header file)

---
+++ Citizen.h
@@ -0,0 +1,35 @@
+#ifndef CITIZEN_H
+#define CITIZEN_H
+
+#include <iostream>
+#include <string>
+
+#include "Building.h"
+#include "Employment.h"
+#include "Satisfaction.h"
+
+class Building;
+
+class Citizen
+{
+    private:
+        std::string name;
+        int age;
+        Employment* employment;
+        Satisfaction* satisfaction;
+        Building* jobBuilding;
+        Building* home;
+
+    public:
+        Citizen(std::string name, int age);
+        void payTax(std::string tax);
+        void startWork();
+        void goHome();
+        Employment* getEmployment();
+        void setEmployment(Employment* e);
+        Satisfaction* getSatisfaction();
+        void setSatisfaction(Satisfaction* s);
+        void getJob();
+};
+
+#endif
