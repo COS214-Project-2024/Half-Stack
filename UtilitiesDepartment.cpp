2135b7b22a9ccd2731de2434e5f2900dd2e2b69a
Sashen <sashengajai.sg@gmail.com>
2024-10-29 02:36:01 +0200

    Create UtilitiesDepartment.cpp

---
+++ UtilitiesDepartment.cpp
@@ -0,0 +1,26 @@
+#include "UtilitiesDepartment.h"
+
+UtilitiesDepartment::UtilitiesDepartment()
+{
+	
+}
+
+void UtilitiesDepartment::supplyWater()
+{
+	std::cout << "The utilities department is supplying water." << std::endl;
+}
+
+void UtilitiesDepartment::supplyEnergy()
+{
+	std::cout << "The utilities department is supplying power." << std::endl;
+}
+
+void UtilitiesDepartment::manageWaste()
+{
+	std::cout << "The utilities department is managing waste." << std::endl;
+}
+
+void UtilitiesDepartment::manageSewage()
+{
+	std::cout << "The utilities department is managing sewage." << std::endl;
+}
