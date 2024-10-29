a6168e9c975ca98c9f55554f34c7928028d8a1c1
DevanDewet <161315233+DevanDewet@users.noreply.github.com>
2024-10-29 12:34:42 +0200

    Update Plant.h
    added member variables

--- Plant.h
+++ Plant.h
@@ -2,12 +2,23 @@
 #define PLANT_H
 
 #include "Industrial.h"
+#include "Citizen.h"
+#include <vector>
+#include <memory>
 
 class Plant : public Industrial
 {
-
-public:
+private:
 	std::string type;
+	int capacity;
+	int occupancy;
+	std::string location;
+
+	//smart pointers for automatic memory management
+	std::vector<std::unique_ptr<Citizen>> citizens; 
+	std::vector<std::unique_ptr<Citizen>> employees;
+public:
+	
 
 	Plant(int num, std::string l);
 
