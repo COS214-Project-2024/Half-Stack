036f3d9f21e5a58bc11304487275be554b9a5fd1
Au5tin-R <srhung1231@gmail.com>
2024-10-29 22:14:30 +0200

    Residential header file
    Added functions for the decorator pattern

---
+++ Residential.h
@@ -0,0 +1,49 @@
+#ifndef RESIDENTIAL_H
+#define RESIDENTIAL_H
+
+#include <vector>
+
+#include "Building.h"
+#include "Citizen.h"
+
+class Residential : public Building
+{
+
+protected:
+	std::vector<Citizen*> residents;
+
+public:		
+
+	Residential(int num,  std::string loc);
+
+	void addCitizen(Citizen* c);
+
+	void removeCitizen(Citizen* c);
+
+	void consumeResources();
+
+	virtual void increaseSatisfaction() = 0 ;
+
+	std::vector<Citizen*> getResidents();
+};
+
+class Estate : public Residential
+{
+
+private:
+	std::vector<Residential*> buildings;
+
+public:
+
+	Estate(int num,  std::string loc);
+
+	void addBuilding(Residential* b);
+
+	void removeBuilding(Residential* b);
+
+	Building* build();
+	void increaseSatisfaction();
+	void consumeResources();
+};
+
+#endif
