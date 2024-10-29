4756603b2bb9e9468ca9d2a0812da8405552b7cc
Tia-H <tiaharripersad@gmail.com>
2024-10-30 01:12:43 +0200

    Update Government.h
    added getters

--- Government.h
+++ Government.h
@@ -1,29 +1,50 @@
 #ifndef GOVERNMENT_H
 #define GOVERNMENT_H
 #include "Department.h"
-#include "TransportDepartment.h"
-#include "LawDepartment.h"
-#include "PublicServicesDepartment.h"
 #include "ResidentialDepartment.h"
+#include "TransportDepartment.h"
 #include "UtilitiesDepartment.h"
+#include "PublicServicesDepartment.h"
+#include "LawDepartment.h"
 #include "CityGrowthDepartment.h"
+#include "ResourceManager.h"
 #include <vector>
 
 class Government : public Department {
 
 private:
-	std::vector<Department*> departmentList;
+	std::vector<Department*> departments;
 	std::string name;
 	static Government* uniqueInstance;
+	std::vector<Citizen*> citizens;
+
+	ResidentialDepartment* resDep;
+	TransportDepartment* transportDep;
+	UtilitiesDepartment* utilityDep;
+	PublicServicesDepartment* psDep;
+	LawDepartment* lawDep;
+	CityGrowthDepartment* cgDep;
+	ResourceManager* resourceManager;
 
 protected:
 	Government(std::string name);
 
+public:
 	static Government* instance(std::string name);
 
-	~Government();
+	ResidentialDepartment* getResidentialDepartment();
+	TransportDepartment* getTransportDepartment();
+	UtilitiesDepartment* getUtilitiesDepartment();
+	PublicServicesDepartment* getPublicServicesDepartment();
+	LawDepartment* getLawDepartment();
+	CityGrowthDepartment* getCityGrowthDepartment();
+	ResourceManager* getResourceManager();
 
-	Government(Government& g);
+	~Government();
+	void addCitizen(Citizen* c);
+	bool hasCitizen(Citizen* c);
+	int getTotalCitizens();
+	int getTotalUnemployed();
 };
 
 #endif
