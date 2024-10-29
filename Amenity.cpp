8feb61c4e75f0aef0ab53e0cd3868bf025ec5fe9
Au5tin-R <srhung1231@gmail.com>
2024-10-29 13:29:02 +0200

    Amenities cpp file

---
+++ Amenity.cpp
@@ -0,0 +1,31 @@
+#include "Amenity.h"
+
+Amenity::Amenity()
+{
+	
+}
+
+void Amenity::increaseSatisfaction()
+{
+	
+}
+
+Garden::Garden()
+{
+
+}
+
+Internet::Internet()
+{
+
+}
+
+Gym::Gym()
+{
+
+}
+
+Pool::Pool()
+{
+
+}
