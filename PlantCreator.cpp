746f23b34e3769631186a9bfc2fa7ac44d6e043d
DevanDewet <161315233+DevanDewet@users.noreply.github.com>
2024-10-29 12:51:15 +0200

    Update PlantCreator.cpp
    PowerPlantCreator creatPlant method

--- PlantCreator.cpp
+++ PlantCreator.cpp
@@ -1,13 +1,27 @@
 #include "PlantCreator.h"
+#include <stdexcept>
 
 PlantCreator::PlantCreator()
 {
 	
 }
 
-Plant* PowerPlantCreator::createPlant()
+Plant* PowerPlantCreator::createPlant(int capacity, const std::string& location)
 {
-	return new PowerPlant(1, "");
+    if (capacity <= 0) 
+    {
+        std::cout << "Error: Capacity must be a positive integer." << std::endl;
+        throw std::invalid_argument("Capacity must be positive.");
+    }
+
+
+    if (location.empty()) 
+    {
+        std::cout << "Error: Location cannot be empty." << std::endl;
+        throw std::invalid_argument("Location cannot be empty.");
+    }
+
+    return new PowerPlant(capacity, location);
 }
 
 Plant* WaterPlantCreator::createPlant()
