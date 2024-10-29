7007aba2b90c22d384255b2d10a42e11e72c0cdc
Sashen <sashengajai.sg@gmail.com>
2024-10-29 02:34:50 +0200

    Create UtilitiesDepartment.h

---
+++ UtilitiesDepartment.h
@@ -0,0 +1,27 @@
+#ifndef UTILITIESDEPARTMENT_H
+#define UTILITIESDEPARTMENT_H
+
+#include "Building.h"
+#include "Department.h"
+#include "UtilityCommand.h"
+
+class UtilitiesDepartment : public Department
+{
+
+private:
+	UtilityCommand* commands[4];
+
+public:
+
+	UtilitiesDepartment();
+
+	void supplyWater();
+
+	void supplyEnergy();
+
+	void manageWaste();
+
+	void manageSewage();
+};
+
+#endif
