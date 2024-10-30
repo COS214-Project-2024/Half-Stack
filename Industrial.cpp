af1512e904e8c0fe596940b5d5670cfb31ad1e0e
Michael_C_Stone <mikestone2002@gmail.com>
2024-10-30 14:27:44 +0200

    Industrial Doxygen

--- Industrial.cpp
+++ Industrial.cpp
@@ -1,16 +1,34 @@
 #include "Industrial.h"
 #include <iostream>
 
+/**
+ * @brief Constructs an Industrial building with the specified parameters.
+ * 
+ * @param num The number associated with the building (e.g., ID).
+ * @param l The location of the building.
+ */
 Industrial::Industrial(int num, std::string l) : Building(num, l)
 {
 	
 }
 
+/**
+ * @brief Adds a Citizen to the Industrial building's workforce.
+ * 
+ * @param c Pointer to the Citizen to be added.
+ */
 void Industrial::addCitizen(Citizen* c)
 {
 	employees.push_back(c);
 }
 
+/**
+ * @brief Removes a Citizen from the Industrial building's workforce.
+ * 
+ * Searches for the specified Citizen and removes them if found.
+ * 
+ * @param c Pointer to the Citizen to be removed.
+ */
 void Industrial::removeCitizen(Citizen* c)
 {
 	for (std::vector<Citizen*>::iterator i = employees.begin(); i != employees.end(); i++)
@@ -23,6 +41,12 @@ void Industrial::removeCitizen(Citizen* c)
     }
 }
 
+/**
+ * @brief Consumes resources required for the operation of the Industrial building.
+ * 
+ * Decreases the resource levels and prints a message indicating whether 
+ * resources are available or if more are needed.
+ */
 void Industrial::consumeResources()
 {
     if (this->resourceManager->decreaseResourceLevels(40, 60, 20, 20, 20) == true)
