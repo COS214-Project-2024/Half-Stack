2f0a0e6340b13e11936ac83788de3f2b11220718
Michael_C_Stone <mikestone2002@gmail.com>
2024-10-30 14:23:29 +0200

    Commercial Doxygen

--- Commercial.cpp
+++ Commercial.cpp
@@ -1,16 +1,36 @@
 #include "Commercial.h"
 #include <iostream>
 
+/**
+ * @brief Constructs a new Commercial object.
+ * 
+ * Initializes a Commercial building with the specified capacity and location.
+ * 
+ * @param num The initial capacity of the Commercial building.
+ * @param l The location of the Commercial building.
+ */
 Commercial::Commercial(int num, std::string l) : Building(num, l)
 {
 	
 }
 
+/**
+ * @brief Adds a Citizen to the employees of the Commercial building.
+ * 
+ * @param c Pointer to the Citizen to be added.
+ */
 void Commercial::addCitizen(Citizen* c)
 {
 	employees.push_back(c);
 }
 
+/**
+ * @brief Removes a Citizen from the employees of the Commercial building.
+ * 
+ * Searches for the specified Citizen and removes them from the employee list.
+ * 
+ * @param c Pointer to the Citizen to be removed.
+ */
 void Commercial::removeCitizen(Citizen* c)
 {
 	for (std::vector<Citizen*>::iterator i = employees.begin(); i != employees.end(); i++)
@@ -23,6 +43,12 @@ void Commercial::removeCitizen(Citizen* c)
     }
 }
 
+/**
+ * @brief Consumes resources required for the operation of the Commercial building.
+ * 
+ * If sufficient resources are available, the building will consume them;
+ * otherwise, a message will indicate that more resources are needed.
+ */
 void Commercial::consumeResources()
 {
     if (this->resourceManager->decreaseResourceLevels(30, 50, 0, 0, 0) == true)
