7dab4025d54a931629db3e2bc0b18584bab7dc16
Michael_C_Stone <mikestone2002@gmail.com>
2024-10-30 14:29:48 +0200

    Landmark Doxygen

--- Landmark.cpp
+++ Landmark.cpp
@@ -1,16 +1,34 @@
 #include "Landmark.h"
 #include <iostream>
 
+/**
+ * @brief Constructs a Landmark building with the specified parameters.
+ * 
+ * @param num The number associated with the building (e.g., ID).
+ * @param l The location of the building.
+ */
 Landmark::Landmark(int num,  std::string l) : Building(num, l)
 {
 	
 }
 
+/**
+ * @brief Adds a Citizen to the Landmark building's workforce.
+ * 
+ * @param c Pointer to the Citizen to be added.
+ */
 void Landmark::addCitizen(Citizen* c)
 {
 	employees.push_back(c);
 }
 
+/**
+ * @brief Removes a Citizen from the Landmark building's workforce.
+ * 
+ * Searches for the specified Citizen and removes them if found.
+ * 
+ * @param c Pointer to the Citizen to be removed.
+ */
 void Landmark::removeCitizen(Citizen* c)
 {
 	for (std::vector<Citizen*>::iterator i = employees.begin(); i != employees.end(); i++)
@@ -23,6 +41,12 @@ void Landmark::removeCitizen(Citizen* c)
     }
 }
 
+/**
+ * @brief Consumes resources required for the operation of the Landmark building.
+ * 
+ * Decreases the resource levels and prints a message indicating whether 
+ * resources are available or if more are needed.
+ */
 void Landmark::consumeResources()
 {
     if (this->resourceManager->decreaseResourceLevels(5, 15, 0, 0, 0) == true)
--- Landmark.h
+++ Landmark.h
@@ -6,20 +6,49 @@
 
 #include "Building.h"
 
+/**
+ * @class Landmark
+ * @brief The Landmark class represents a landmark building that employs citizens 
+ * and consumes resources.
+ */
 class Landmark : public Building
 {
 
 private:
+	///< A vector of pointers to the citizens employed by this Landmark.
 	std::vector<Citizen*> employees;
 
 public:
-
+	/**
+     * @brief Constructs a Landmark building with the specified parameters.
+     * 
+     * @param num The number associated with the building (e.g., ID).
+     * @param loc The location of the building.
+     */
 	Landmark(int num, std::string loc);
 
+	/**
+     * @brief Adds a Citizen to the Landmark building's workforce.
+     * 
+     * @param c Pointer to the Citizen to be added.
+     */
 	void addCitizen(Citizen* c);
 
+	/**
+     * @brief Removes a Citizen from the Landmark building's workforce.
+     * 
+     * Searches for the specified Citizen and removes them if found.
+     * 
+     * @param c Pointer to the Citizen to be removed.
+     */
 	void removeCitizen(Citizen* c);
 
+	/**
+     * @brief Consumes resources required for the operation of the Landmark building.
+     * 
+     * Decreases the resource levels and prints a message indicating whether 
+     * resources are available or if more are needed.
+     */
 	void consumeResources();
 };
 
