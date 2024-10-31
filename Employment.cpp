109d66e510d2b227ee6d09628f492f094d145450
Au5tin-R <srhung1231@gmail.com>
2024-11-01 01:20:11 +0200

    Update Employment.cpp
    Added Doxygen

--- Employment.cpp
+++ Employment.cpp
@@ -1,21 +1,37 @@
 #include "Employment.h"
 
+/**
+ * @brief Retrieves the status of the employment.
+ * @return A string indicating that the citizen is employed.
+ */
 std::string Employed::getStatus()
 {
-    return "Employed";
+    return "Employed"; // Returns the employment status as "Employed"
 }
 
-Employment *Employed::toggleStatus()
+/**
+ * @brief Toggles the employment status to unemployed.
+ * @return A pointer to an Unemployed object.
+ */
+Employment* Employed::toggleStatus()
 {
-    return new Unemployed();
+    return new Unemployed(); // Toggles the status to Unemployed
 }
 
+/**
+ * @brief Retrieves the status of the employment.
+ * @return A string indicating that the citizen is unemployed.
+ */
 std::string Unemployed::getStatus()
 {
-    return "Unemployed";
+    return "Unemployed"; // Returns the employment status as "Unemployed"
 }
 
-Employment *Unemployed::toggleStatus()
+/**
+ * @brief Toggles the employment status to employed.
+ * @return A pointer to an Employed object.
+ */
+Employment* Unemployed::toggleStatus()
 {
-    return new Employed();
+    return new Employed(); // Toggles the status to Employed
 }
