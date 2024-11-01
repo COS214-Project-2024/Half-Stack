1d226e572e15634b2db6b80cbc26813455ccad46
Sashen <sashengajai.sg@gmail.com>
2024-11-01 02:45:51 +0200

    Doxygen Comments

--- Department.cpp
+++ Department.cpp
@@ -1,15 +1,26 @@
 #include "Department.h"
 
+/**
+ * @brief Constructor for the Department class.
+ */
 Department::Department()
 {
 	
 }
 
+/**
+ * @brief Adds a building to the vector of department buildings.
+ * @param b Pointer to the building.
+ */
 void Department::addBuilding(Building* b) 
 {
 	buildings.push_back(b);
 }
 
+/**
+ * @brief Removes a building from the vector of department buildings.
+ * @param b Pointer to the building.
+ */
 void Department::removeBuilding(Building* b) 
 {
 	for (std::vector<Building*>::iterator i = buildings.begin(); i != buildings.end(); i++)
