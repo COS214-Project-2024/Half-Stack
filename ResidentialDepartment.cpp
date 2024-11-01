cb7601b680c8d3780a99db2d18d029d9b04e3eda
Sashen <sashengajai.sg@gmail.com>
2024-11-01 02:41:55 +0200

    Doxygen Comments

--- ResidentialDepartment.cpp
+++ ResidentialDepartment.cpp
@@ -2,11 +2,18 @@
 
 ResidentialDepartment* ResidentialDepartment::uniqueInstance;
 
+/**
+ * @brief Constructor for the ResidentialDepartment class.
+ */
 ResidentialDepartment::ResidentialDepartment()
 {
 	
 }
 
+/** 
+ * @brief Singleton instance of ResidentialDepartment.
+ * @return Pointer to the instance of ResidentialDepartment.
+ */
 ResidentialDepartment* ResidentialDepartment::instance()
 {
 	if (uniqueInstance == NULL)
@@ -16,11 +23,19 @@ ResidentialDepartment* ResidentialDepartment::instance()
     return uniqueInstance;
 }
 
+/**
+ * @brief Adds a building to the vector of residential buildings.
+ * @param b Pointer to the building.
+ */
 void ResidentialDepartment::addBuilding(Building* b)
 {
     residents.push_back(b);
 }
 
+/**
+ * @brief Removes a building to the vector of residential buildings.
+ * @param b Pointer to the building.
+ */
 void ResidentialDepartment::removeBuilding(Building* b)
 {
 	for (std::vector<Building*>::iterator i = residents.begin(); i != residents.end(); i++)
@@ -33,6 +48,10 @@ void ResidentialDepartment::removeBuilding(Building* b)
     }	
 }
 
+/**
+ * @brief Calculates the number of residential buildings.
+ * @return Number of residential buildings.
+ */
 int ResidentialDepartment::getTotalBuildings()
 {
     int counter = 0;
@@ -43,6 +62,10 @@ int ResidentialDepartment::getTotalBuildings()
     return counter;
 }
 
+/**
+ * @brief Calculates the number of possible citizens in the residential buildings.
+ * @return Number of possible citizens.
+ */
 int ResidentialDepartment::getTotalCapacity()
 {
     int total = 0;
@@ -51,57 +74,4 @@ int ResidentialDepartment::getTotalCapacity()
         total = total + (*i)->capacity;
     }
     return total;
-}
-
-void ResidentialDepartment::attachCitizen(Citizen* c) 
-{
-    Observingcitizens.push_back(c);
-}
-
-void ResidentialDepartment::detachCitizen(Citizen* c)
-{
-    for (auto it = Observingcitizens.begin(); it != Observingcitizens.end(); ++it) 
-	{
-        if (*it == c) 
-		{
-            Observingcitizens.erase(it);
-            return;
-        }
-    }
-}
-
-void ResidentialDepartment::setLoadShedding(bool status, const std::string& startTime, const std::string& endTime) 
-{
-    if (isLoadShedding != status) 
-    {
-        isLoadShedding = status;
-
-        std::string message;
-
-        if (status) 
-        {
-            message = "Load-shedding started from " + startTime + " to " + endTime + ".";
-        } 
-        else 
-        {
-            message = "Load-shedding has ended.";
-        }
-
-        notifyCitizens(message);
-    } 
-    else 
-    {
-      
-        std::cout << "Load-shedding status is already set to " 
-                  << (status ? "ON" : "OFF") 
-                  << ". No change made." << std::endl;
-    }
-}
-
-void ResidentialDepartment::notifyCitizens(const std::string& message) 
-{
-    for (auto* citizen : Observingcitizens) 
-	{
-        citizen->receiveNotification(message);	
-    }
 }
