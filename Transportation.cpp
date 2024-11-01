9f8bdc23985f3cd1cf8ed095522799255d3fc921
Sashen <sashengajai.sg@gmail.com>
2024-11-01 02:39:27 +0200

    Doxygen Comments

--- Transportation.cpp
+++ Transportation.cpp
@@ -5,21 +5,6 @@ Transportation::Transportation()
 
 }
 
-void Transportation::open()
-{
-	isOpen = true;
-}
-
-void Transportation::close()
-{
-    isOpen = false;
-}
-
-bool Transportation::getStatus()
-{
-    return isOpen;
-}
-
 Airport::Airport()
 {
 
@@ -40,36 +25,90 @@ Trail::Trail()
 
 }
 
-void Trail::open()
-{
-    clear();
-}
-
-void Trail::close()
-{
-    block();
-}
-
-bool Trail::getStatus()
-{
-    return getPathStatus();
-}
-
 Pathway::Pathway()
 {
 
 }
 
+/**
+ * @brief Opens the transport to be used by citizens.
+ * (Target of Adapter)
+ */
+void Transportation::open()
+{
+	isOpen = true;
+}
+
+/**
+ * @brief Closes the transport from being used by citizens.
+ * (Target of Adapter)
+ */
+void Transportation::close()
+{
+    isOpen = false;
+}
+
+/**
+ * @brief Whether the transport is open or closed.
+ * (Target of Adapter)
+ * @return Status of the respective transportation mode.
+ */
+bool Transportation::getStatus()
+{
+    return isOpen;
+}
+
+/**
+ * @brief Opens the trail to be used by citizens.
+ * (Adapter of Adapter)
+ */
+void Trail::open()
+{
+    clear();
+}
+
+/**
+ * @brief Closes the trail from being used by citizens.
+ * (Adapter of Adapter)
+ */
+void Trail::close()
+{
+    block();
+}
+
+/**
+ * @brief Whether the trail is open or closed.
+ * (Adapter of Adapter)
+ * @return Status of the trail mode.
+ */
+bool Trail::getStatus()
+{
+    return getPathStatus();
+}
+
+/**
+ * @brief Clears the pathway to be accessed.
+ * (Adaptee of Adapter)
+ */
 void Pathway::clear()
 {
     path = true;
 }
 
+/**
+ * @brief Blocks the pathway from being accessed.
+ * (Adaptee of Adapter)
+ */
 void Pathway::block()
 {
     path = false;
 }
 
+/**
+ * @brief Whether the pathway is cleared or blocked.
+ * (Adaptee of Adapter)
+ * @return Status of the pathway.
+ */
 bool Pathway::getPathStatus()
 {
     return path;
