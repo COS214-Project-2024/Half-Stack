e43e1086d49560ea2deb188196c17a460e6a7c07
Au5tin-R <srhung1231@gmail.com>
2024-11-01 02:02:11 +0200

    Update Amenity.h
    Added Doxygen

--- Amenity.h
+++ Amenity.h
@@ -1,60 +1,160 @@
 #ifndef RESIDENTIALDECORATOR_H
 #define RESIDENTIALDECORATOR_H
 
-#include"Residential.h"
+#include "Residential.h"
 
+/**
+ * @class Amenity
+ * @brief A base decorator class for adding amenities to a residential area.
+ * 
+ * This class provides a way to decorate a Residential object with additional features
+ * like increased satisfaction and resource consumption.
+ */
 class Amenity : public Residential
 {
-
 protected:
-	Residential* amenity;
-	
-public:
-	Amenity(Residential* amen);
-	virtual ~Amenity();
+    Residential* amenity; ///< Pointer to the decorated Residential object.
 
-	void increaseSatisfaction();
-	void consumeResources();
+public:
+    /**
+     * @brief Constructs an Amenity decorator.
+     * @param amen Pointer to the Residential object to decorate.
+     */
+    Amenity(Residential* amen);
+
+    /**
+     * @brief Destroys the Amenity decorator and cleans up the decorated Residential object.
+     */
+    virtual ~Amenity();
+
+    /**
+     * @brief Increases the satisfaction level of residents.
+     * 
+     * This function calls the increaseSatisfaction method of the decorated Residential object.
+     */
+    void increaseSatisfaction();
+
+    /**
+     * @brief Consumes resources associated with the amenity.
+     * 
+     * This function calls the consumeResources method of the decorated Residential object.
+     */
+    void consumeResources();
 };
 
+/**
+ * @class Garden
+ * @brief A decorator that adds a Garden amenity to a Residential object.
+ */
 class Garden : public Amenity
 {
-
 public:
-	Garden(Residential* amen);
-	virtual ~Garden();
-	void consumeResources() override;
+    /**
+     * @brief Constructs a Garden decorator.
+     * @param amen Pointer to the Residential object to decorate.
+     */
+    Garden(Residential* amen);
+
+    /**
+     * @brief Destroys the Garden decorator.
+     */
+    virtual ~Garden();
+
+    /**
+     * @brief Consumes extra water resources for the Garden.
+     */
+    void consumeResources() override;
+
+    /**
+     * @brief Increases the satisfaction level of residents due to the Garden.
+     */
     void increaseSatisfaction() override;
 };
 
+/**
+ * @class Internet
+ * @brief A decorator that adds Internet service to a Residential object.
+ */
 class Internet : public Amenity
 {
-
 public:
-	Internet(Residential* amen);
-	virtual ~Internet();
-	void consumeResources() override;
+    /**
+     * @brief Constructs an Internet decorator.
+     * @param amen Pointer to the Residential object to decorate.
+     */
+    Internet(Residential* amen);
+
+    /**
+     * @brief Destroys the Internet decorator.
+     */
+    virtual ~Internet();
+
+    /**
+     * @brief Consumes extra power resources for the Internet service.
+     */
+    void consumeResources() override;
+
+    /**
+     * @brief Increases the satisfaction level of residents due to Internet service.
+     */
     void increaseSatisfaction() override;
-	
 };
 
+/**
+ * @class Gym
+ * @brief A decorator that adds a Gym facility to a Residential object.
+ */
 class Gym : public Amenity
 {
-
 public:
-	Gym(Residential* amen);
-	virtual ~Gym();
-	void consumeResources() override;
+    /**
+     * @brief Constructs a Gym decorator.
+     * @param amen Pointer to the Residential object to decorate.
+     */
+    Gym(Residential* amen);
+
+    /**
+     * @brief Destroys the Gym decorator.
+     */
+    virtual ~Gym();
+
+    /**
+     * @brief Consumes extra electricity resources for the Gym.
+     */
+    void consumeResources() override;
+
+    /**
+     * @brief Increases the satisfaction level of residents due to the Gym.
+     */
     void increaseSatisfaction() override;
 };
 
+/**
+ * @class Pool
+ * @brief A decorator that adds a Swimming Pool to a Residential object.
+ */
 class Pool : public Amenity
 {
-
 public:
-	Pool(Residential* amen);
-	virtual ~Pool();
-	void consumeResources() override;
+    /**
+     * @brief Constructs a Pool decorator.
+     * @param amen Pointer to the Residential object to decorate.
+     */
+    Pool(Residential* amen);
+
+    /**
+     * @brief Destroys the Pool decorator.
+     */
+    virtual ~Pool();
+
+    /**
+     * @brief Consumes extra water resources for the Pool.
+     */
+    void consumeResources() override;
+
+    /**
+     * @brief Increases the satisfaction level of residents due to the Pool.
+     */
     void increaseSatisfaction() override;
 };
 
