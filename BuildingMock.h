8f6e45838b3daf56d0378fcf65cf424e9202c67d
Michael_C_Stone <mikestone2002@gmail.com>
2024-11-05 00:22:45 +0200

    Moving all unit testing files to Unit Testing Folder

--- BuildingMock.h
+++
@@ -1,8 +0,0 @@
-#include <gmock/gmock.h>
-#include "Building.h"
-
-class MockBuilding : public Building {
-public:
-    MockBuilding(int num, std::string l) : Building(num, l) {}
-    MOCK_METHOD(void, consumeResources, (), (override));
-};
