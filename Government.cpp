fc970b40fea98a32b00fd9ed136aecc783b48f96
Keanu Ellary <u22893459@tuks.co.za>
2024-11-04 12:41:17 +0200

    Doxygen comments

--- Government.cpp
+++ Government.cpp
@@ -4,6 +4,14 @@
 
 Government* Government::uniqueInstance;
 
+/** @brief Constructs the Government object. 
+ * 
+ * @param name
+ * This param is the users name, they own their city so the government should be theirs
+ * Sets the different Departments needed in this city with the singleton instances that each class provides. 
+ * A message is outputted stating that the user runs this city.
+ * The departments can now observe changes, accompany for resources, budgets and more
+*/ 
 Government::Government(std::string name) 
 {
 	this->name = name;
@@ -23,35 +31,81 @@ Government::Government(std::string name)
 	std::cout << name << " runs this city." <<std::endl;
 }
 
+/**
+ * @brief Gets the Residential Department instance
+ * 
+ * @return The Residential Department instance that the class holds
+ */
 ResidentialDepartment* Government::getResidentialDepartment()
 {
 	return resDep;
 }
+
+/**
+ * @brief Gets the Transport Department instance
+ * 
+ * @return The Transport Department instance that the class holds
+ */
 TransportDepartment* Government::getTransportDepartment()
 {
 	return transportDep;
 }
+
+/**
+ * @brief Gets the Utilities Department instance
+ * 
+ * @return The Utilities Department instance that the class holds
+ */
 UtilitiesDepartment* Government::getUtilitiesDepartment()
 {
 	return utilityDep;
 }
+
+/**
+ * @brief Gets the Public Services Department instance
+ * 
+ * @return The Public Services Department instance that the class holds
+ */
 PublicServicesDepartment* Government::getPublicServicesDepartment()
 {
 	return psDep;
 }
+
+/**
+ * @brief Gets the Law Department instance
+ * 
+ * @return The Law Department instance that the class holds
+ */
 LawDepartment* Government::getLawDepartment()
 {
 	return lawDep;
 }
+
+/**
+ * @brief Gets the City Growth Department instance
+ * 
+ * @return The City Growth Department instance that the class holds
+ */
 CityGrowthDepartment* Government::getCityGrowthDepartment()
 {
 	return cgDep;
 }
+
+/**
+ * @brief Gets the Resource Manager instance
+ * 
+ * @return The Resource Manager instance that the class holds
+ */
 ResourceManager* Government::getResourceManager()
 {
 	return resourceManager;
 }
 
+/**
+ * @brief Sets the Government singleton instance to ensure that only one instance is created
+ * 
+ * @return Returns the instance. Now we can use the Government functionality through this instance
+ */
 Government* Government::instance(std::string name) 
 {
 	if(uniqueInstance == nullptr)
@@ -62,6 +116,12 @@ Government* Government::instance(std::string name)
 	return uniqueInstance;
 }
 
+/**
+ * @brief Destructs and deallocates any dynamic memory created by the Government class
+ * 
+ * Clears and deletes all citizens in the Citizen vector
+ * Deletes the instance, effectively cleaning up memory
+ */
 Government::~Government()
 {
 	std::vector<Citizen*>::iterator it = citizens.begin();
@@ -75,7 +135,27 @@ Government::~Government()
 	delete uniqueInstance;
 }
 
-
+/**
+ * @brief This function adds a new ctitizen to the Government accoriding to certain conditions
+ * 
+ * We first check is the number of homeless citizens exceeds a limit of 20. 
+ * If there are too many homless citizens, the new citizen will not be added and the object will be deleted
+ * Checks if the citizen does not already exist within the Government
+ * If the homeless count is not 20 and its a new citizen, the citizen is added and additional updates are then triggered to the Residential and City Growth departments
+ * 
+ * @param c Pointer to the object that holds the new citizen to be added
+ * 
+ * If the number of homeless people exceed 20:
+ * - an error message is displayed, c is deleted and we exit
+ * If the citizen is already registered:
+ * - message is displayed to the user, exits
+ * If the citizen is successfully added:
+ * - Citizen is added to the list
+ * - Resource Department is called to accompany this citizen
+ * - City Growth department is called to update the numbers
+ * 
+ * @note The functions accompanies for memory leaks by deleting the Citizen object if unsuccessful addition occured 
+ */
 void Government::addCitizen(Citizen* c)
 {
 	int homeless= getHomeless();
@@ -95,6 +175,19 @@ void Government::addCitizen(Citizen* c)
 	cgDep->updatePopulation();
 }
 
+
+/** 
+ * @brief Checks whether or not the citizen already exists within the list of citizens
+ * 
+ * iterates through thr vector and checks whether the citizen already exists
+ * 
+ * @param c Pointer to the citizen object we want to test existance
+ * 
+ * @return Boolean value of either true or false
+ * True if the citizen does exist
+ * False if the citizen does not exist
+ * 
+*/ 
 bool Government::hasCitizen(Citizen* c)
 {
 	std::vector<Citizen*>::iterator it = citizens.begin();
@@ -109,6 +202,12 @@ bool Government::hasCitizen(Citizen* c)
 	return false;
 }
 
+/**  
+ * @brief Calculates the total number of citizens currently in the list
+ * 
+ * @return
+ * The cumulutive number of citizens within the list
+*/ 
 int Government::getTotalCitizens()
 {
 	int total=0;
@@ -121,6 +220,12 @@ int Government::getTotalCitizens()
 	return total;
 }
 
+/**  
+ * @brief Calculates the total number of citizens that are unemployed that are currently in the list
+ * 
+ * @return
+ * The cumulutive number of unemployed citizens within the list
+*/ 
 int Government::getTotalUnemployed()
 {
 	int total=0;
@@ -136,6 +241,12 @@ int Government::getTotalUnemployed()
 	return total;
 }
 
+/**  
+ * @brief Calculates the total number of employed citizens currently in the list
+ * 
+ * @return
+ * The cumulutive number of employed citizens within the list
+*/ 
 int Government::getTotalEmployed()
 {
 	int total=0;
@@ -151,11 +262,22 @@ int Government::getTotalEmployed()
 	return total;
 }
 
+/**
+ * @brief Gets the list of citizens currently registered in the system
+ * 
+ * @return The vector that holds the citizens registered
+ */
 std::vector<Citizen*> Government::getCitizens() const 
 {
     return citizens;
 }
 
+/**  
+ * @brief Calculates the total number of homeless citizens currently in the list
+ * 
+ * @return
+ * The cumulutive number of homeless citizens within the list
+*/ 
 int Government::getHomeless()
 {
 	int total=0;
@@ -170,6 +292,12 @@ int Government::getHomeless()
 	return total;
 }
 
+/**  
+ * @brief Calculates the total number of citizens who's satisfactions are neutral that arecurrently in the list
+ * 
+ * @return
+ * The cumulutive number of neutral citizens within the list
+*/ 
 int Government::getTotalNeutral()
 {
 	int total=0;
@@ -184,6 +312,13 @@ int Government::getTotalNeutral()
 
 	return total;
 }
+
+/**  
+ * @brief Calculates the total number of citizens who's satisfactions are satisfied that arecurrently in the list
+ * 
+ * @return
+ * The cumulutive number of satisfied citizens within the list
+*/ 
 int Government::getTotalSatisfied()
 {
 	int total=0;
@@ -199,6 +334,12 @@ int Government::getTotalSatisfied()
 	return total;
 }
 
+/**  
+ * @brief Calculates the total number of citizens who's satisfactions are unsatisfied that arecurrently in the list
+ * 
+ * @return
+ * The cumulutive number of unsatisfied citizens within the list
+*/ 
 int Government::getTotalUnsatisfied()
 {
 	int total=0;
@@ -214,7 +355,26 @@ int Government::getTotalUnsatisfied()
 	return total;
 }
 
-
+/**
+ * @brief Displays to the user the current numbers of the various types of citizens, resource numbers and building types that currently exists
+ * 
+ * Outputs to the user the following in the sequence:
+ * Population: Total number of citizens
+ * Homeless: Total homeless citizens
+ * Employed: Total employed citizens
+ * , Unemployed: Total unemployed citizens
+ * Satisfied: Total Satisfied citizens, Neutral: Total neutral citizens, Unsatisfied: Total unsatisfied citizens
+ * Budget: Gets the budget from the resource manager
+ * Water: Gets the water level from resource manager
+ * , Energy: Gets the energy level from resource manager
+ * , Wood: Gets the number of wood from resource manager
+ * , Steel: Gets the number of steel from resource manager
+ * , General Materials: Gets the number of general materials from resource manager
+ * Residental Buildings: Total number of buildings from Residential Department [capacity: total capacity for the residential buildings]
+ * Job Buildings: Total number of job buildings from Public services Department [capacity: total capacity for the job buildings]
+ * Airports: Total airports, Roads : Total roads, Railways: Total railways
+ * WaterPlants: Total waterplants, PowerPlants: Total powerplants
+ */
 void Government::getCityStats()
 {
 	std::cout << "<<---------------------------- CITY STATS ---------------------------->>" <<std::endl;
--- Government.h
+++ Government.h
@@ -10,51 +10,219 @@
 #include "ResourceManager.h"
 #include <vector>
 
-class Government : public Department {
+class Government : public Department
+{
 
 private:
-	//std::vector<Department*> departments;
+	// std::vector<Department*> departments;
 	std::string name;
-	static Government* uniqueInstance;
-	std::vector<Citizen*> citizens;
+	static Government *uniqueInstance;
+	std::vector<Citizen *> citizens;
 
-	ResidentialDepartment* resDep;
-	TransportDepartment* transportDep;
-	UtilitiesDepartment* utilityDep;
-	PublicServicesDepartment* psDep;
-	LawDepartment* lawDep;
-	CityGrowthDepartment* cgDep;
-	ResourceManager* resourceManager;
+	ResidentialDepartment *resDep;
+	TransportDepartment *transportDep;
+	UtilitiesDepartment *utilityDep;
+	PublicServicesDepartment *psDep;
+	LawDepartment *lawDep;
+	CityGrowthDepartment *cgDep;
+	ResourceManager *resourceManager;
 
 protected:
+	/** @brief Constructs the Government object.
+	 *
+	 * @param name
+	 * This param is the users name, they own their city so the government should be theirs
+	 * Sets the different Departments needed in this city with the singleton instances that each class provides.
+	 * A message is outputted stating that the user runs this city.
+	 * The departments can now observe changes, accompany for resources, budgets and more
+	 */
 	Government(std::string name);
 
 public:
-	static Government* instance(std::string name);
+	/**
+	 * @brief Sets the Government singleton instance to ensure that only one instance is created
+	 *
+	 * @return Returns the instance. Now we can use the Government functionality through this instance
+	 */
+	static Government *instance(std::string name);
 
-	ResidentialDepartment* getResidentialDepartment();
-	TransportDepartment* getTransportDepartment();
-	UtilitiesDepartment* getUtilitiesDepartment();
-	PublicServicesDepartment* getPublicServicesDepartment();
-	LawDepartment* getLawDepartment();
-	CityGrowthDepartment* getCityGrowthDepartment();
-	ResourceManager* getResourceManager();
+	/**
+	 * @brief Gets the Residential Department instance
+	 *
+	 * @return The Residential Department instance that the class holds
+	 */
+	ResidentialDepartment *getResidentialDepartment();
 
+	/**
+	 * @brief Gets the Transport Department instance
+	 *
+	 * @return The Transport Department instance that the class holds
+	 */
+	TransportDepartment *getTransportDepartment();
+
+	/**
+	 * @brief Gets the Utilities Department instance
+	 *
+	 * @return The Utilities Department instance that the class holds
+	 */
+	UtilitiesDepartment *getUtilitiesDepartment();
+
+	/**
+	 * @brief Gets the Public Services Department instance
+	 *
+	 * @return The Public Services Department instance that the class holds
+	 */
+	PublicServicesDepartment *getPublicServicesDepartment();
+
+	/**
+	 * @brief Gets the Law Department instance
+	 *
+	 * @return The Law Department instance that the class holds
+	 */
+	LawDepartment *getLawDepartment();
+
+	/**
+	 * @brief Gets the City Growth Department instance
+	 *
+	 * @return The City Growth Department instance that the class holds
+	 */
+	CityGrowthDepartment *getCityGrowthDepartment();
+
+	/**
+	 * @brief Gets the Resource Manager instance
+	 *
+	 * @return The Resource Manager instance that the class holds
+	 */
+	ResourceManager *getResourceManager();
+
+	/**
+	 * @brief Destructs and deallocates any dynamic memory created by the Government class
+	 *
+	 * Clears and deletes all citizens in the Citizen vector
+	 * Deletes the instance, effectively cleaning up memory
+	 */
 	~Government();
 
-	void addCitizen(Citizen* c);
-	bool hasCitizen(Citizen* c);
-	int getTotalCitizens();
-	std::vector<Citizen*> getCitizens() const;
+	/**
+	 * @brief This function adds a new ctitizen to the Government accoriding to certain conditions
+	 *
+	 * We first check is the number of homeless citizens exceeds a limit of 20.
+	 * If there are too many homless citizens, the new citizen will not be added and the object will be deleted
+	 * Checks if the citizen does not already exist within the Government
+	 * If the homeless count is not 20 and its a new citizen, the citizen is added and additional updates are then triggered to the Residential and City Growth departments
+	 *
+	 * @param c Pointer to the object that holds the new citizen to be added
+	 *
+	 * If the number of homeless people exceed 20:
+	 * - an error message is displayed, c is deleted and we exit
+	 * If the citizen is already registered:
+	 * - message is displayed to the user, exits
+	 * If the citizen is successfully added:
+	 * - Citizen is added to the list
+	 * - Resource Department is called to accompany this citizen
+	 * - City Growth department is called to update the numbers
+	 *
+	 * @note The functions accompanies for memory leaks by deleting the Citizen object if unsuccessful addition occured
+	 */
+	void addCitizen(Citizen *c);
 
+	/**
+	 * @brief Checks whether or not the citizen already exists within the list of citizens
+	 *
+	 * iterates through thr vector and checks whether the citizen already exists
+	 *
+	 * @param c Pointer to the citizen object we want to test existance
+	 *
+	 * @return Boolean value of either true or false
+	 * True if the citizen does exist
+	 * False if the citizen does not exist
+	 *
+	 */
+	bool hasCitizen(Citizen *c);
+
+	/**
+	 * @brief Calculates the total number of citizens currently in the list
+	 *
+	 * @return
+	 * The cumulutive number of citizens within the list
+	 */
+	int getTotalCitizens();
+
+	/**
+	 * @brief Gets the list of citizens currently registered in the system
+	 *
+	 * @return The vector that holds the citizens registered
+	 */
+	std::vector<Citizen *> getCitizens() const;
+
+	/**
+	 * @brief Calculates the total number of citizens that are unemployed that are currently in the list
+	 *
+	 * @return
+	 * The cumulutive number of unemployed citizens within the list
+	 */
 	int getTotalUnemployed();
+
+	/**
+	 * @brief Calculates the total number of employed citizens currently in the list
+	 *
+	 * @return
+	 * The cumulutive number of employed citizens within the list
+	 */
 	int getTotalEmployed();
+
+	/**
+	 * @brief Calculates the total number of homeless citizens currently in the list
+	 *
+	 * @return
+	 * The cumulutive number of homeless citizens within the list
+	 */
 	int getHomeless();
 
+	/**
+	 * @brief Calculates the total number of citizens who's satisfactions are neutral that arecurrently in the list
+	 *
+	 * @return
+	 * The cumulutive number of neutral citizens within the list
+	 */
 	int getTotalNeutral();
+
+	/**
+	 * @brief Calculates the total number of citizens who's satisfactions are satisfied that arecurrently in the list
+	 *
+	 * @return
+	 * The cumulutive number of satisfied citizens within the list
+	 */
 	int getTotalSatisfied();
+
+	/**
+	 * @brief Calculates the total number of citizens who's satisfactions are unsatisfied that arecurrently in the list
+	 *
+	 * @return
+	 * The cumulutive number of unsatisfied citizens within the list
+	 */
 	int getTotalUnsatisfied();
 
+	/**
+	 * @brief Displays to the user the current numbers of the various types of citizens, resource numbers and building types that currently exists
+	 *
+	 * Outputs to the user the following in the sequence:
+	 * Population: Total number of citizens
+	 * Homeless: Total homeless citizens
+	 * Employed: Total employed citizens
+	 * , Unemployed: Total unemployed citizens
+	 * Satisfied: Total Satisfied citizens, Neutral: Total neutral citizens, Unsatisfied: Total unsatisfied citizens
+	 * Budget: Gets the budget from the resource manager
+	 * Water: Gets the water level from resource manager
+	 * , Energy: Gets the energy level from resource manager
+	 * , Wood: Gets the number of wood from resource manager
+	 * , Steel: Gets the number of steel from resource manager
+	 * , General Materials: Gets the number of general materials from resource manager
+	 * Residental Buildings: Total number of buildings from Residential Department [capacity: total capacity for the residential buildings]
+	 * Job Buildings: Total number of job buildings from Public services Department [capacity: total capacity for the job buildings]
+	 * Airports: Total airports, Roads : Total roads, Railways: Total railways
+	 * WaterPlants: Total waterplants, PowerPlants: Total powerplants
+	 */
 	void getCityStats();
 };
 
